a3c609abb346781926b442fd1d109645c70ab4fd
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2020-10-14 10:28:43 +0200

    Ajoutez des fichiers projet.

---
+++ TestMccBoard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MccDaq;
+using LibCapteurAn;
+
+namespace TP_Analogique
+{
+    class TestMccBoard
+    {
+        static void Main(string[] args)
+        {
+            ushort dataValue = 0;
+            Range range = Range.Bip10Volts;
+            MccBoard mccBoard7 = new MccBoard(0);
+            for(int i = 0; i < 200; i++)
+            {
+                Console.WriteLine(dataValue);
+                var ULStat = mccBoard7.AIn(7, range, out dataValue);
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
