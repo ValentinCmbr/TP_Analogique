d939eeec9c73c29ec231f8bde224d472ca6b8cc5
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2020-10-20 09:42:59 +0200

    2.1

--- TestMccBoard/Program.cs
+++ TestMccBoard/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MccDaq;
 using LibCapteurAn;
+using System.Security.Authentication.ExtendedProtection;
+using System.Threading;
 
 namespace TP_Analogique
 {
@@ -12,17 +14,36 @@ namespace TP_Analogique
     {
         static void Main(string[] args)
         {
-            ushort dataValue = 0;
+            float option = 0;
+            ushort dataValueLum = 0;
+            float dataValueLumVolt = 0;
+            ushort dataValueTemp = 0;
+            float dataValueTempVolt = 0;
+            int channelLum = 0;
+            int channelTemp = 0;
             Range range = Range.Bip10Volts;
-            MccBoard mccBoard7 = new MccBoard(0);
+            MccBoard mccBoard = new MccBoard(0);
             for(int i = 0; i < 200; i++)
             {
-                Console.WriteLine(dataValue);
-                var ULStat = mccBoard7.AIn(7, range, out dataValue);
-                Console.ReadKey();
-            }
-            
+                channelLum = 7;
+                channelTemp = 6;
 
+                mccBoard.AIn(channelLum, range, out dataValueLum);
+                mccBoard.VIn(channelLum, range, out dataValueLumVolt,0);
+
+
+                mccBoard.AIn(channelTemp, range, out dataValueTemp);
+                mccBoard.VIn(channelTemp, range, out dataValueTempVolt, 0);
+
+                Console.WriteLine( "DataLum: " + dataValueLum.ToString());
+                Console.WriteLine(dataValueLumVolt + "Volts");
+                Console.WriteLine("DataTemp: " + dataValueTemp.ToString());
+                Console.WriteLine(dataValueTempVolt + "Volts");
+
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadKey();
         }
     }
 }
